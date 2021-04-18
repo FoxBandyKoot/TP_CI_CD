f9dc12d7e389fb28f614c6affc321ad337566e03
maximegrandynov <maxime.grand@ynov.com>
2021-04-18 16:32:16 +0200

    init install and create unit test

---
+++ test/test.js
@@ -0,0 +1,11 @@
+const express = require ('express'); 
+const chai = require ('chai'); 
+const request = require ('supertest');
+
+const app = express ();
+
+describe ('POST test ...', () => { 
+    it ('should create ...', () => { 
+        // code pour tester l'API
+}); 
+});
